c3aafac01177881f665d39e2749f51f503921f3d
fibi1736-create <fibi1736@gmail.com>
2026-04-18 15:11:35 +0500

    Initial commit: Flask practice task app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homePage():
+<<<<<<< HEAD
     subjects = ["English", "Science", "Math"]
     fruits = ["Apple", "Banana", "Mango", "Orange", "Cherry"]
     nums = [1, 2, 3, 4, 5]
@@ -109,3 +110,43 @@ def resultPage():
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
+=======
+    return render_template("home.html")
+
+@app.route("/result", methods=["POST", "GET"])
+def resultPage():
+    name   = request.form.get("name")
+    type   = request.form.get("type")
+    num    = int(request.form.get("num") or 1)
+    method = request.form.get("method")
+
+    if type == "classic":
+        price = 3000
+    elif type == "deluxe":
+        price = 5000
+    elif type == "junior":
+        price = 7000
+    elif type == "grand":
+        price = 10000
+    elif type == "penthouse":
+        price = 15000
+    else:
+        price = 0
+
+    total = num * price
+
+    if method == "cash":
+        discount = total * 0.05
+    else:
+        discount = 0
+
+    total = total - discount
+
+    return render_template("result.html",
+        name=name, type=type, num=num,
+        method=method, price=price, total=total
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+>>>>>>> 001b93e (Initial commit: Flask practice task app)
